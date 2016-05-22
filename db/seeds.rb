526f87e09d7df3566a04aa19b483865ec566af29
Dennise Saxton <dennisepnd@gmail.com>
2016-05-22 09:43:41 -0500

    Added sample beers through seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+beers = Beer.create([
+  { name: 'Kostritzer', description: 'Dark but smooth black lager'},
+  { name: 'Spaten Lager', description: 'Smooth and easy to drink'},
+  { name: 'Stiegl Radler', description: 'Sweet and refreshing'}
+  ])
